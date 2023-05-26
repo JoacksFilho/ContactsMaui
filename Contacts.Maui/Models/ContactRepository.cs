6513c9f868f5f8a268dd44fa43fc4d1e48766eb9
joacksFilhoAgger <joacksfilho@agger.com.br>
2023-05-26 16:02:52 -0300

    Adicionar arquivos de projeto.

---
+++ Contacts.Maui/Models/ContactRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Contacts.Maui.Models
+{
+    public static class ContactRepository
+    {
+        public static List<ContactModel> _contacts = new List<ContactModel>()
+        {
+            new ContactModel { Name = "Joacks Filho", Email = "joackslemos@hotmail.com"},
+            new ContactModel { Name = "Heitor Vinícius", Email = "Heitor@hotmail.com"},
+            new ContactModel { Name = "Thais Pereira", Email = "Thaislemos@hotmail.com"},
+            new ContactModel { Name = "Lara Santello", Email = "Laralemos@hotmail.com"},
+        };
+
+        public static List<ContactModel> GetContacts() => _contacts;
+
+        public static ContactModel GetContactById(int contactId)
+        {
+            return _contacts.FirstOrDefault(c => c.ContactId == contactId);
+        }
+    }
+}
---
+++ Contacts.Maui/Views/ContactsPage.xaml.cs
@@ -0,0 +1,39 @@
+using Contacts.Maui.Models;
+namespace Contacts.Maui.Views;
+
+public partial class ContactsPage : ContentPage
+{
+	public ContactsPage()
+	{
+		InitializeComponent();
+
+        List<ContactModel> contacts = ContactRepository.GetContacts(); 
+			
+		listContacts.ItemsSource = contacts;
+	}
+
+
+    private async void listContacts_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+		if(listContacts.SelectedItem != null)
+        {
+			//logic
+			await Shell.Current.GoToAsync(nameof(EditContactPage));		
+		}	
+    }
+
+    private void listContacts_ItemTapped(object sender, ItemTappedEventArgs e)
+    {
+		listContacts.SelectedItem = null;
+    }
+
+    //private void btnEditContact_Cliked(object sender, EventArgs e)
+    //{
+    //	Shell.Current.GoToAsync(nameof(EditContactPage));
+    //}
+
+    //   private void btnAddContact_Clicked(object sender, EventArgs e)
+    //   {
+    //       Shell.Current.GoToAsync(nameof(AddContactPage));
+    //   }
+}
